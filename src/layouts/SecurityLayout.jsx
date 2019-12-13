a71d566ff089fd2b993306ea63e475fd3a642225
liangfaliang <535615816@qq.com>
2019-12-13 16:16:03 +0800

    init

--- src/layouts/SecurityLayout.jsx
+++ src/layouts/SecurityLayout.jsx
@@ -13,20 +13,21 @@ class SecurityLayout extends React.Component {
     this.setState({
       isReady: true,
     });
-    const { dispatch } = this.props;
-
-    if (dispatch) {
-      dispatch({
-        type: 'user/fetchCurrent',
-      });
-    }
+    // const { dispatch } = this.props;
+    //
+    // if (dispatch) {
+    //   dispatch({
+    //     type: 'user/fetchCurrent',
+    //   });
+    // }
   }
 
   render() {
     const { isReady } = this.state;
     const { children, loading, currentUser } = this.props; // You can replace it to your authentication rule (such as check token exists)
     // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
-    const isLogin = currentUser && currentUser.id;
+    // const isLogin = currentUser && currentUser.id;
+    const isLogin = !!localStorage.getItem('X-Token');
     const queryString = stringify({
       redirect: window.location.href,
     });
