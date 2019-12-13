38a3d59b084ca88aef440f6a538eef46479f00f7
houxm <hximeng@zhonglanmedia.com>
2019-12-13 17:56:44 +0800

    ..

--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
 const configs = {
   development: {
-    API_SERVER: 'http://localhost:8008',
+    API_SERVER: 'http://localhost:8080',
   },
   sit: {
     API_SERVER: 'https://mass.zhihuizhan.net/api',
